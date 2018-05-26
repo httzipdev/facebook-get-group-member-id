fda0b3a5173ac5eabd6c24958743bfb37eeaa220
HUYNH THANH TUNG <httzip@gmail.com>
2018-05-26 14:19:35 +0700

    Update MTF

--- get.php
+++ get.php
@@ -1,29 +1,39 @@
 <?php
-// Coded by HTTZIP
+// coded by HTTZIP
 // Share on httzip.com
 function getid($link)
 {
-	$get = file_get_contents($link);
-	$decode = json_decode($get);
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => $link,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_SSL_VERIFYHOST => false
+	));
+	$get = curl_exec($curl);
+	curl_close($curl);
+
+	$decode = json_decode($get,JSON_UNESCAPED_UNICODE);
 	
-	$count = count($decode->data);
+	$count = count($decode['data']);
 	$i= 0;
-	foreach ($decode->data as $data) {
-		$file = 'list.txt';
-		$current = file_get_contents($file);
-		$current .= $data->id."\n";
-		file_put_contents($file, $current);
+	foreach ($decode['data'] as $data) {
+		
+		$file = fopen($list, 'a');
+		fwrite($file, $data['id']."\n");
+		fclose($file);
 		$i++;
 		
 		if($i == $count)
 		{
-			if(empty($link = $decode->paging->next))
+			if(empty($link = $decode['paging']['next']))
 			{
 				echo "Hoàn tất ";
 				return false;
 			}
 			else{
-				$link = $decode->paging->next;
+				$link = $decode['paging']['next'];
 				return getid($link);
 			}
 			
@@ -33,10 +43,9 @@ function getid($link)
 if(!empty($_GET['id']))
 {
 $token = ""; // Access_Token
-$limit = "500"; // Số lượng UID sẽ được lấy mỗi lượt foreach
-
+$limit = "400"; // So Luong ID moi lan lay , cang nho cang it ton CPU
 getid("https://graph.facebook.com/".$_GET['id']."/members?fields=id&limit=".$limit."&access_token=".$token);
 }else{
-	echo "Invail Group ID";
+	echo "Invaild Group ID";
 }
 
